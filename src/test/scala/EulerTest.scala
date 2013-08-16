286216867549e1551799c475dea23b3fa6382da2
Philip Morris <philip.morris@springer.com>
2013-08-16 20:11:52 +0100

    Problem 1

--- src/test/scala/EulerTest.scala
+++ src/test/scala/EulerTest.scala
@@ -4,8 +4,8 @@ import org.scalatest.junit.JUnitRunner
 import org.scalatest.matchers.ShouldMatchers
 
 @RunWith(classOf[JUnitRunner])
-class FirstTest extends FunSpec with ShouldMatchers {
-  it("should pass") {
-    true should be(true)
+class EulerTest extends FunSpec with ShouldMatchers {
+  it("Problem 1 - Sum Multiples of 3 and 5") {
+    println((1 until 1000).filter(x => (x % 3 == 0 | x % 5 == 0)).sum)
   }
 }
