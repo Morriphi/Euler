1abed41444e3b1fb208f03af89e5568df194c818
Philip Morris <philip.morris@springer.com>
2013-08-16 20:43:08 +0100

    Problem 2

--- src/test/scala/EulerTest.scala
+++ src/test/scala/EulerTest.scala
@@ -8,4 +8,9 @@ class EulerTest extends FunSpec with ShouldMatchers {
   it("Problem 1 - Sum Multiples of 3 and 5") {
     println((1 until 1000).filter(x => (x % 3 == 0 | x % 5 == 0)).sum)
   }
+
+  it("Problem 2 - Even Fibonacci Numbers") {
+    def fib(x: Int, y: Int) : Stream[Int] = x #:: fib(y, x+y)
+    println(fib(1, 2).takeWhile(_ < 4000000).filter(_ % 2 == 0).sum)
+  }
 }
